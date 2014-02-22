2ea7163b37784bfb8ce893262feafb74dfdab868
Humberto <humbhenri@gmail.com>
2014-02-22 10:14:19 -0300

    draw implemented

--- main/main.go
+++ main/main.go
@@ -1,41 +1,83 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	. "github.com/humbhenri/tic_tac_toe"
-	"net/http"
 	"io/ioutil"
-	"encoding/json"
+	"net/http"
 )
 
 type End struct {
-    winner Mark
+	Winner Mark
+    Board Board
 }
 
 func Json(b Board) (s string) {
-    js, err := json.Marshal(b)
-    if err != nil {
-        fmt.Println(err)
-        s = ""
-        return
-    }
-    s = string(js)
-    return
+	js, err := json.Marshal(b)
+	if err != nil {
+		fmt.Println(err)
+		s = ""
+		return
+	}
+	s = string(js)
+	return
 }
 
 func swapPlayer(player Mark) Mark {
-    if player == X {
-        return O
-    } else if player == O {
-        return X
-    }
-    return None
+	if player == X {
+		return O
+	} else if player == O {
+		return X
+	}
+	return None
+}
+
+func getMarkFromPlayer(body []byte) (int, int) {
+	var cell interface{}
+	err := json.Unmarshal(body, &cell)
+	if err != nil {
+		panic(err)
+	}
+
+	pos := cell.(map[string]interface{})["pos"]
+	row := pos.([]interface{})[0]
+	col := pos.([]interface{})[1]
+	return int(row.(float64)), int(col.(float64))
+}
+
+func sendJson(w http.ResponseWriter, st interface{}) {
+	json, err := json.Marshal(st)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(json))
+	fmt.Println(string(json))
+}
+
+func gameEnded(b *Board) (bool, Mark) {
+	end := false
+	win := b.Win()
+	if win != None || b.FreePositions() == 0 {
+		end = true
+	}
+	return end, win
+}
+
+func sendEndMessageIfGameOver(w http.ResponseWriter, b *Board) bool {
+	end, mark := gameEnded(b)
+	if end {
+		sendJson(w, End{mark, *b})
+		return true
+	}
+	return false
 }
 
 func main() {
 	b := &Board{}
 	b.Start()
-    player := X
+	player := X
 
 	fmt.Println("Listening on port 8080.")
 
@@ -49,34 +91,26 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		var cell interface{}
-		err = json.Unmarshal(body, &cell)
+
+		row, col := getMarkFromPlayer(body)
+		fmt.Println(row, col)
+		b.Put(player, row, col)
+		if sendEndMessageIfGameOver(w, b) {
+			return
+		}
+		player = swapPlayer(player)
+		err = Play(b, player)
 		if err != nil {
 			panic(err)
+		} else {
+			sendJson(w, *b)
 		}
+		if sendEndMessageIfGameOver(w, b) {
+			return
+		}
+		player = swapPlayer(player)
+		fmt.Println(b.String())
 
-		pos := cell.(map[string]interface{})["pos"]
-		row := pos.([]interface{})[0]
-		col := pos.([]interface{})[1]
-		b.Put(player, int(row.(float64)), int(col.(float64)))
-        player = swapPlayer(player)
-        err = Play(b, player)
-        win := b.Win()
-        if err != nil {
-            fmt.Println(err)
-        } else {
-            if win == None {
-                w.Header().Set("Content-Type", "application/json")
-                fmt.Fprint(w, Json(*b))
-            } else {
-                w.Header().Set("Content-Type", "application/json")
-                fmt.Fprint(w, End{win})
-            }
-
-        }
-        player = swapPlayer(player)
-            
-        fmt.Println(b.String())
 	})
 
 	http.ListenAndServe(":8080", nil)
